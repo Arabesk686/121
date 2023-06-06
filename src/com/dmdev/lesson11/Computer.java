df361f0a3013ba81e88d149d69fc9162dcd42454
Arabesk686 <zadolbalsy_@mail.ru>
2023-06-06 21:21:47 +0300

    lesson

---
+++ src/com/dmdev/lesson11/Computer.java
@@ -0,0 +1,34 @@
+package com.dmdev.lesson11;
+
+public class Computer {
+
+    private Ssd ssd;
+    private Ram ram;
+
+    {
+        System.out.println("init block computer");
+    }
+    static {
+        System.out.println("static block computer");
+    }
+
+    public Computer(Ssd ssd, Ram ram) {
+        this.ssd = ssd;
+        this.ram = ram;
+    }
+
+    public Computer(){
+        System.out.println("Constructor computer");
+    }
+    public void load(){
+        System.out.println("Я загрузился");
+    }
+
+    public Ssd getSsd() {
+        return ssd;
+    }
+
+    public Ram getRam() {
+        return ram;
+    }
+}
